27078aa5e971a0e58b480cb691e61ae4b8df3ffa
KisaraF <kisarawenu@gmail.com>
2026-05-10 07:03:55 +0530

    change ingestion strategy, remove process logs

--- ingestion_modules/ingestion_zip.py
+++ ingestion_modules/ingestion_zip.py
@@ -1,17 +1,11 @@
-import requests
+import requests, requests.exceptions as req_err
 from io import BytesIO
 from zipfile import ZipFile
 from databricks.connect import DatabricksSession
 import pandas as pd
 from pyspark.sql.functions import col, sha2, concat_ws, current_timestamp
+from fetch_http_link import operation
 
-
-# Constants
-links = {
-        "2025_q3":"https://fis.fda.gov/content/Exports/faers_ascii_2025q3.zip"
-}
-
-sample_file = "sample.zip"
 
 def sparkSession() -> DatabricksSession:
     spark = DatabricksSession.builder.serverless().profile("DEFAULT").getOrCreate()
@@ -19,29 +13,29 @@ def sparkSession() -> DatabricksSession:
 
 
 def checkStatus(url: str):
-    res = requests.head(url)
-
-    msg = f"Response ok? {res.ok}! Status code -> {res.status_code}"
-    print(msg)
+    res = requests.get(url)
+    return res
 
 
-def readFile(ss: DatabricksSession, url: str):
+def readFile(ss: DatabricksSession, url):
+    print("Processing starting ...")
     reserved_words = ["DEMO", "DRUG", "OUTC", "REAC"]
 
-    with open(url, 'rb') as bin_f:
-        content = bin_f.read()
-
-    with ZipFile(BytesIO(content)) as zip_file:
-        chosen_files = [f for f in zip_file.namelist() if f.endswith('.txt') and any(word in f for word in reserved_words)]
+    with ZipFile(BytesIO(url)) as zip_file:
+        chosen_files = [
+            f
+            for f in zip_file.namelist()
+            if f.endswith(".txt") and any(word in f for word in reserved_words)
+        ]
         print(chosen_files)
 
         for file in chosen_files:
             print(f"\nProcessing {file}")
-            
+
             tb_id = file.split("/")[-1].split(".")[0][:4].lower()
             tb_name = f"pediatric_adr_events.raw_source.{tb_id}"
-            
-            df = pd.read_csv(zip_file.open(file), sep='$', dtype=str)
+
+            df = pd.read_csv(zip_file.open(file), sep="$", dtype=str)
             print(f"File loaded to pandas {df.shape}")
 
             spark_df = ss.createDataFrame(df)
@@ -49,24 +43,28 @@ def readFile(ss: DatabricksSession, url: str):
 
             spark_df = spark_df.withColumn(
                 "_hash_id",
-                sha2(concat_ws("||", *[col(c) for c in spark_df.columns]), 256)
+                sha2(concat_ws("||", *[col(c) for c in spark_df.columns]), 256),
             )
-            
-            spark_df = spark_df.withColumn(
-                "ingestion_timestamp",
-                current_timestamp()
-            )
+
+            spark_df = spark_df.withColumn("ingestion_timestamp", current_timestamp())
             print("Added metadata columns")
 
             spark_df.write.format("delta").mode("append").saveAsTable(tb_name)
             print("Loaded to Databricks")
-        
-
 
 
 if __name__ == "__main__":
-    #checkStatus(links["2025_q3"])
-
-    spark = sparkSession()
-    print('spark session created')
-    readFile(spark, sample_file)
+    url = operation()
+    print(f"Retrieved the URL: {url}")
+    try:
+        print("Extracting bytes from URL")
+        res = requests.get(url)
+        res.raise_for_status()
+        spark = sparkSession()
+        print("spark session created")
+        readFile(spark, res.content)
+        print(type(res.content))
+    except req_err.HTTPError as http_err:
+        print(f"HTTP Error: {http_err}")
+    except Exception as e:
+        print(f"Other error encountered: {e}")
