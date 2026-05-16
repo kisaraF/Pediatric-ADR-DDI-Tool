36c6471f776fa8715e2113929b9790c5f36c9d4b
KisaraF <kisarawenu@gmail.com>
2026-05-17 00:08:16 +0530

    add RxNorm source to dbt, revamp int__drugs

--- pediatric_adr_ddi_transforms/pediatric_adr_ddi_transform/models/intermediate/int__drug.sql
+++ pediatric_adr_ddi_transforms/pediatric_adr_ddi_transform/models/intermediate/int__drug.sql
@@ -17,55 +17,14 @@ with base as (
   )
 )
 ,
-lookup_1 as (
-  select  
-    drugname,
-    prod_ai
-  from base
-  qualify row_number() over (partition by drugname, prod_ai order by prod_ai) = 1
-)
-,
-lookup_counts as (
-  select 
-    drugname, 
-    count(drugname) as drug_count
-  from lookup_1
-  group by 1
-  having drug_count > 1
-)
-,
-filtered_lookup as (
-  select 
-    *,
-    length(prod_ai) as wc_len
-  from lookup_1
-  where drugname not in (
-    select distinct drugname
-    from lookup_counts
-  )
-  and prod_ai is not null
-)
-,
-filtered_lookup_final as (
-  select 
-    drugname,
-    coalesce(prod_ai, 'NA') as prod_ai
-  from filtered_lookup
-  qualify dense_rank() over(partition by drugname order by wc_len desc) <> 2
-)
-, 
-final as (
-    select 
-    base.primaryid, 
-    base.caseid, 
-    base.drug_seq, 
-    base.role_cod, 
-    flf.drugname, 
-    flf.prod_ai
-    from base
-    left join filtered_lookup_final flf
-    on base.drugname = flf.drugname
+-- Use RxNorm table and join it's info
+rx_norm as (
+    select distinct upper(drug_raw) as drug_name, in_name
+    from {{ source('raw', 'rxnorm_key') }}
+    where drug_raw <> "dummy"
 )
 
-select *
-from final
+select base.*, rx_norm.in_name as rxnorm_in
+from base
+left join rx_norm
+on base.drugname = rx_norm.drug_name
