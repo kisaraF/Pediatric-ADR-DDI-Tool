4cda679c88c3de1793791207f13a249fe4adba0f
KisaraF <kisarawenu@gmail.com>
2026-05-26 00:21:05 +0530

    ARM & processing complete

---
+++ pediatric_adr_ddi_transforms/pediatric_adr_ddi_transform/models/marts/mined_rules_filtered.sql
@@ -0,0 +1,20 @@
+{{ config(tags=["marts", "rules_mined_filtered"]) }}
+
+with base_filtering as (
+    select
+        *,
+        size(filter(antecedent, z -> z ilike 'reac_%')) as reac_count
+    from {{ source('mined_rules_db', 'mined_rules') }}
+    where
+        exists(antecedent, x -> x ilike 'drug_%')
+        and exists(antecedent, x -> x ilike 'demo_age_%')
+        and exists(antecedent, x -> x ilike 'demo_sex_%')
+        and exists(antecedent, x -> x ilike 'demo_weight_%')
+        and not exists(antecedent, x -> x ilike 'outc_%')
+        and not exists(consequent, y -> y ilike 'outc_%')
+)
+
+select *
+from base_filtering
+where confidence < 1
+order by lift desc, reac_count desc
