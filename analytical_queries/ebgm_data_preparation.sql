180931fcbe7bbc29828b22aa9008496d3334d01d
KisaraF <kisarawenu@gmail.com>
2026-06-06 23:16:13 +0530

    fixed total N count in the ebgm query

--- analytical_queries/ebgm_data_preparation.sql
+++ analytical_queries/ebgm_data_preparation.sql
@@ -6,6 +6,11 @@ with base as (
 	from pediatric_adr_events.dbt_transforms_arm_data.arm__transactions
 )
 ,
+Total_Count as ( 
+	select count(distinct primaryid) as total_N
+	from pediatric_adr_events.dbt_transforms_arm_data.arm__transactions
+)
+,
 N_count as (
 	select drug, reaction, count(distinct primaryid) as N_
 	from base
@@ -25,7 +30,7 @@ Reaction_count as (
 )
 ,
 combined_base as (
-	select base.*, d.drug_count, r.reaction_count, tot.N_
+	select base.*, d.drug_count, r.reaction_count, tot.N_, t.total_N
 	from base
 	left join Drug_count d 
 	on d.drug = base.drug
@@ -34,9 +39,13 @@ combined_base as (
 	left join N_count tot
 	on tot.reaction = base.reaction 
 	and tot.drug = base.drug
+	cross join Total_Count t
 )
 
-select 
-	*,
-	round((drug_count * reaction_count) / N_,2) as E_
-from combined_base
+select distinct h.* 
+from (
+	select 
+		* except(primaryid),
+		round((drug_count * reaction_count) / total_N,2) as E_
+	from combined_base
+) h
